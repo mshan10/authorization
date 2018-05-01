2c6700ed4b4b512c391453ce856ee352a6ee1d50
mshan10 <Jack@Jacks-MacBook-Pro.local>
2018-05-01 18:46:06 -0400

    edited authservice

--- src/app/common/app.module.js
+++ src/app/common/app.module.js
@@ -2,3 +2,7 @@ angular
   .module('common', [
     'ui.router'
   ]);
+  // .run(function ($transitions, cfpLoadingBar) {
+  //   $transitions.onStart){}, cfpLoadingBar.start);
+  //   $transitions.onSuccess({}, cfpLoadingBar.complete);
+  // });
--- src/app/components/auth/auth.service.js
+++ src/app/components/auth/auth.service.js
@@ -1,45 +1,74 @@
 function AuthService($parseAuth) {
-  // var auth = $firebaseAuth();
-  // var authData = null;
-  // function storeAuthData(response) {
-  //   authData = response;
-  //   return authData;
-  // }
-  // function onSignIn(user) {
-  //   authData = user;
-  //   return auth.$requireSignIn();
-  // }
-  // function clearAuthData() {
-  //   authData = null;
-  // }
+  this.register = function (user) {
+    var userData = new Parse.Object('Account')
+    Parse.defineAttributes(userData, ['email', 'password'])
+    userData.email = user.email;
+    userData.password = user.password;
+
+    userData.save(null, {
+
+      success: function(userData) {
+      },
+      error: function(userData, error) {
+
+      }
+    });
+  }
   // this.login = function (user) {
-  //   return auth
-  //     .$signInWithEmailAndPassword(user.email, user.password)
-  //     .then(storeAuthData);
-  // };
-  // this.register = function (user) {
-  //   return auth
-  //     .$createUserWithEmailAndPassword(user.email, user.password)
-  //     .then(storeAuthData);
-  // };
-  // this.logout = function () {
-  //   return auth
-  //     .$signOut()
-  //     .then(clearAuthData);
-  // };
-  // this.requireAuthentication = function () {
-  //   return auth
-  //     .$waitForSignIn().then(onSignIn);
-  // };
-  // this.isAuthenticated = function () {
-  //   return !!authData;
-  // };
-  // this.getUser = function () {
-  //   if (authData) {
-  //     return authData;
-  //   }
-  // };
+  //   var Account = Parse.Object.extend("Account")
+  //   var query = new Parse.Query(Account)
+  //   query.include("email")
+  //   query.include("password")
+  // }
 }
+
+
+
+
+
+
+//
+//   var auth = $firebaseAuth();
+//   var authData = null;
+//   function storeAuthData(response) {
+//     authData = response;
+//     return authData;
+//   }
+//   function onSignIn(user) {
+//     authData = user;
+//     return auth.$requireSignIn();
+//   }
+//   function clearAuthData() {
+//     authData = null;
+//   }
+//   this.login = function (user) {
+//     return auth
+//       .$signInWithEmailAndPassword(user.email, user.password)
+//       .then(storeAuthData);
+//   };
+//   this.register = function (user) {
+//     return auth
+//       .$createUserWithEmailAndPassword(user.email, user.password)
+//       .then(storeAuthData);
+//   };
+//   this.logout = function () {
+//     return auth
+//       .$signOut()
+//       .then(clearAuthData);
+//   };
+//   this.requireAuthentication = function () {
+//     return auth
+//       .$waitForSignIn().then(onSignIn);
+//   };
+//   this.isAuthenticated = function () {
+//     return !!authData;
+//   };
+//   this.getUser = function () {
+//     if (authData) {
+//       return authData;
+//     }
+//   };
+// }
 
 angular
   .module('components.auth')

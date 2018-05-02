32b17853f959eeaa821152b9343db8551d4ce19b
mshan10 <mshan@nd.edu>
2018-05-02 13:22:08 -0400

    updated auth module

--- src/app/components/auth/auth.module.js
+++ src/app/components/auth/auth.module.js
@@ -10,23 +10,24 @@ angular
     $ParseProvider.initialize(MY_PARSE_APP_ID, MY_PARSE_JS_KEY);
     $ParseProvider.serverURL = 'https://parseapi.back4app.com/';
   }]);
-  // .run(function ($transitions, $state, AuthService) {
-  //   $transitions.onStart({
-  //     to: function (state) {
-  //       return !!(state.data && state.data.requiredAuth);
-  //     }
-  //   }, function () {
-  //     return AuthService
-  //       .requireAuthentication()
-  //       .catch(function () {
-  //         return $state.target('auth.login');
-  //       });
-  //   });
-  //   $transitions.onStart({
-  //     to: 'auth.*'
-  //   }, function () {
-  //     if (AuthService.isAuthenticated()) {
-  //       return $state.target('app');
-  //     }
-  //   });
-  // });
+
+  .run(function ($transitions, $state, AuthService) {
+    $transitions.onStart({
+      to: function (state) {
+        return !!(state.data && state.data.requiredAuth);
+      }
+    }, function () {
+      return AuthService
+        .requireAuthentication()
+        .catch(function () {
+          return $state.target('auth.login');
+        });
+    });
+    $transitions.onStart({
+      to: 'auth.*'
+    }, function () {
+      if (AuthService.isAuthenticated()) {
+        return $state.target('app');
+      }
+    });
+  });

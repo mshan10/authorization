5d01c081445b1c8feb16c043286b9ee4f3199ef2
mshan10 <mshan@nd.edu>
2018-05-02 20:31:29 -0400

    testing login and register

--- src/app/components/auth/auth.service.js
+++ src/app/components/auth/auth.service.js
@@ -5,13 +5,13 @@ function AuthService(Parse) {
     authData = response;
     return authData;
   }
-  function onSignIn(user) {
-    authData = user;
-    return auth.$requireSignIn();
-  }
-  function clearAuthData() {
-    authData = null;
-  }
+  // function onSignIn(user) {
+  //   authData = user;
+  //   return auth.$requireSignIn();
+  // }
+  // function clearAuthData() {
+  //   authData = null;
+  // }
 
   this.register = function (user) {
     return auth
@@ -45,53 +45,22 @@ function AuthService(Parse) {
       var currentUser = Parse.User.current();
     })
     .then(clearAuthData);
+  };
+  //
+  // this.requireAuthentication = function () {
+  //   return auth
+  //     .$waitForSignIn().then(onSignIn);
+  // };
+  //
+  // this.isAuthenticated = function () {
+  //   return !!authData;
+  // };
+  // this.getUser = function () {
+  //   if (authData) {
+  //     return authData;
+  //   }
+  // };
 };
-
-
-
-
-//
-//   var auth = $firebaseAuth();
-//   var authData = null;
-//   function storeAuthData(response) {
-//     authData = response;
-//     return authData;
-//   }
-//   function onSignIn(user) {
-//     authData = user;
-//     return auth.$requireSignIn();
-//   }
-//   function clearAuthData() {
-//     authData = null;
-//   }
-//   this.login = function (user) {
-//     return auth
-//       .$signInWithEmailAndPassword(user.email, user.password)
-//       .then(storeAuthData);
-//   };
-//   this.register = function (user) {
-//     return auth
-//       .$createUserWithEmailAndPassword(user.email, user.password)
-//       .then(storeAuthData);
-//   };
-//   this.logout = function () {
-//     return auth
-//       .$signOut()
-//       .then(clearAuthData);
-//   };
-//   this.requireAuthentication = function () {
-//     return auth
-//       .$waitForSignIn().then(onSignIn);
-//   };
-//   this.isAuthenticated = function () {
-//     return !!authData;
-//   };
-//   this.getUser = function () {
-//     if (authData) {
-//       return authData;
-//     }
-//   };
-// }
 
 angular
   .module('components.auth')

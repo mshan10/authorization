9d29b19bc3d8eace078e0a769ea81f72f3806d31
mshan10 <mshan@nd.edu>
2018-05-03 16:46:14 -0400

    changed clearauthdata

--- src/app/components/auth/auth.service.js
+++ src/app/components/auth/auth.service.js
@@ -10,7 +10,7 @@ function AuthService(Parse) {
   //   return auth.$requireSignIn();
   // }
   function clearAuthData() {
-    authData = null;
+    authData = Parse.User.current();
   }
 
   this.register = function (user) {
@@ -42,9 +42,9 @@ function AuthService(Parse) {
       .then(storeAuthData);
   };
 
-  // this.logout = function () {
-  //   return Parse.User.logOut().then(clearAuthData);
-  // };
+  this.logout = function () {
+    return Parse.User.logOut().then(clearAuthData);
+  };
   //
   // this.requireAuthentication = function () {
   //   return auth

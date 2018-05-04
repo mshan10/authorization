2f1e6fadb87eea29ee145691d1eba359148459c0
mshan10 <mshan@nd.edu>
2018-05-04 17:42:21 -0400

    added additional comments

--- src/app/components/auth/auth.service.js
+++ src/app/components/auth/auth.service.js
@@ -1,20 +1,33 @@
+/**
+ * A service that performs authorization functions
+ * @param  {object} Parse  uses Parse
+ * @param  {type}   var    auth creates new Parse.User()
+ * @param  {type}   var    sets the current user as null
+ */
 function AuthService(Parse) {
   var auth = new Parse.User();
   var currentUser = null;
+
+  /**
+   * A function that stores the currentUser as the current user
+   */
   function storeAuthData(response) {
     currentUser = response;
     return currentUser;
   }
-
+  /**
+   * A function that clears the currentUser and sets it to null
+   */
   function clearAuthData() {
     currentUser = Parse.User.current();
     return currentUser;
   }
   /**
    * Takes user object from the form and stores the email and password
-   * @type   {string} user.email     email
-   * @type   {string} user.password     password
-   * @returns {object} auth
+   * @param   {object} user           takes user from form submition
+   * @param   {string} user.email     email
+   * @param   {string} user.password  password
+   * @returns {object}                auth
    */
   this.register = function (user) {
     auth.set("username", user.email);
@@ -32,7 +45,12 @@ function AuthService(Parse) {
         })
         .then(storeAuthData);
   };
-
+  /**
+   * Takes user object from the form and calls Parse.User.logIn function which takes in a username and password
+   * @type   {string} user.email     email
+   * @type   {string} user.password     password
+   * .then
+   */
   this.login = function (user) {
     return Parse.User
       .logIn(user.email, user.password, {
